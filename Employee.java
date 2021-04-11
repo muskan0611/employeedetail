b9a80861c145aec7218387dcdbf604d3d45b3cd1
muskan0611 <81763309+muskan0611@users.noreply.github.com>
2021-04-11 20:19:32 +0530

    created Employee.java
    This block of code will print Employees data on the screen.

---
+++ Employee.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.String;
+class Employee{
+	public static void main(String[] args){
+		String name,add;
+		int yr;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Name\tYear of joining\t\tAddress");
+		System.out.print("Robert\t\t");
+		System.out.print("1994\t\t");
+		System.out.print("64C-WallsStreet");
+		System.out.println();
+		System.out.print("Sam\t\t");
+		System.out.print("2000\t\t");
+		System.out.print("68D-WallsStreet");
+		System.out.println();
+		System.out.print("John\t\t");
+		System.out.print("1999\t\t");
+		System.out.print("26B-WallsStreet");
+		System.out.println();
+		
+	}
+}
